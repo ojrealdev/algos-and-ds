9cd43289065125873c90215c5fff0b26740af24f
soojwang <soojwang@safaricom.co.ke>
2023-07-14 08:58:45 +0300

    non-whitespace

--- matchWhitespaceRegex.js
+++ matchWhitespaceRegex.js
@@ -1,3 +1,4 @@
 let sample = "Whitespace is important in separating words";
-let countWhiteSpace = /\s/g; // Change this line
+let countWhiteSpace = /\s/g; // find whitesoaces
+let countNonWhiteSpace = /\S/g; 
 let result = sample.match(countWhiteSpace);
